eb3300678ab870c836692c38e283123bdb7ca9b0
H4Y6 <89087786+H4Y6@users.noreply.github.com>
2022-01-15 18:44:10 +0200

    task 1 & 2
    task 1 & 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,31 +1,11 @@
-// const identedListItemQuantity = document.body.firstElementChild.nextElementSibling.children.length;
-// console.log(`Number of categories: ${identedListItemQuantity}`);
+const categoryCount = document.querySelector("#categories").childElementCount;
+const h2 = document.querySelectorAll("h2");
+const subList = document.querySelectorAll(".item > ul");
 
-// const firstIdentedListItemTitle = document.body.firstElementChild.nextElementSibling.children[0].firstChild.nextElementSibling.textContent;
-// console.log(`Category: ${firstIdentedListItemTitle}`);
-// =====================================================================================
+console.log(`Number of categories: ${categoryCount}`);
 
-
-const identedListItemCount = document.querySelector("#categories").childElementCount
-console.log(`Number of categories: ${identedListItemCount}`);
-console.log(' ');
-
-const firstSubtitle = document.querySelector("#categories").children[0].firstChild.nextElementSibling.textContent;
-console.log(`Category: ${firstSubtitle}`);
-
-const firstSublistCount = document.querySelector(".item").children[1].childElementCount;      
-console.log(`Elements: ${firstSublistCount}`);
-console.log(' ');
-
-const secondSubtitle = document.querySelector("#categories").children[1].firstChild.nextElementSibling.textContent;
-console.log(`Category: ${secondSubtitle}`);
-
-const secondSublistCount = document.querySelectorAll(".item")[1].children[1].childElementCount;      
-console.log(`Elements: ${secondSublistCount}`);
-console.log(' ');
-
-const thirdSubtitle = document.querySelector("#categories").children[2].firstChild.nextElementSibling.textContent;
-console.log(`Category: ${thirdSubtitle}`);
-
-const thirdSublistCount = document.querySelectorAll(".item")[2].children[1].childElementCount;      
-console.log(`Elements: ${thirdSublistCount}`);
+for (let i = 0; i < categoryCount; i += 1) {
+  console.log(' ');
+  console.log(`Category: ${h2[i].textContent}`  );
+  console.log(`Elements: ${subList[i].childElementCount}`)
+}
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+
+const items = [];
+ingredients.forEach(function (ingredient) {
+  const item = (document.createElement("li"));
+  item.classList.add('item'); 
+  item.textContent = ingredient;
+  items.push(item);
+});
+list.prepend(...items);
+console.log(list);
