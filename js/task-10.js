2c83262f37b8be25e2176f04ad531ab90ff6cade
H4Y6 <89087786+H4Y6@users.noreply.github.com>
2022-01-19 00:30:36 +0200

    Update task-10.js
    arrow function simplified

--- js/task-10.js
+++ js/task-10.js
@@ -6,9 +6,7 @@ const boxesRef = document.querySelector('#boxes');
 const btnCreateRef = document.querySelector('button[data-create]');
 const btnDestroyRef = document.querySelector('button[data-destroy]');
 btnDestroyRef.addEventListener('click', destroyBoxes);
-btnCreateRef.addEventListener('click', () => {const number = inputRef.value;
-    createBoxes(number);
-});
+btnCreateRef.addEventListener('click', () => createBoxes(inputRef.value));
 function createBoxes(amount) {
     const boxes = [];
     for (let i = 30; i < 30 + amount * 10; i += 10) {
