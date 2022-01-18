d5378da083b099e5b27095e0389dfec1ceec571e
H4Y6 <89087786+H4Y6@users.noreply.github.com>
2022-01-18 21:59:19 +0200

    Update task-10.js
    last task added

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const inputRef = document.querySelector('#controls > input');
+const boxesRef = document.querySelector('#boxes');
+const btnCreateRef = document.querySelector('button[data-create]');
+const btnDestroyRef = document.querySelector('button[data-destroy]');
+btnDestroyRef.addEventListener('click', destroyBoxes);
+btnCreateRef.addEventListener('click', () => {const number = inputRef.value;
+    createBoxes(number);
+});
+function createBoxes(amount) {
+    const boxes = [];
+    for (let i = 30; i < 30 + amount * 10; i += 10) {
+        const boxe = document.createElement("div");
+        boxe.style.width = `${i}px`;
+        boxe.style.height = `${i}px`;
+        boxe.style.backgroundColor = getRandomHexColor();
+        boxes.push(boxe);
+    };
+    return boxesRef.append(...boxes);
+};
+function destroyBoxes(){inputRef.value = '';
+    boxesRef.textContent = '';
+};
